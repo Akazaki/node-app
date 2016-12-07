2bab727aca01c09a61993861a5c49aa1dbb1f53d
hiroki.akazaki_gpol <hiroki.akazaki@gpol.co.jp>
2016-12-07 15:12:21 +0900

    [add] views設置

---
+++ app.js
@@ -0,0 +1,15 @@
+/* 1. expressモジュールをロードし、インスタンス化してappに代入。*/
+var express = require("express");
+var app = express();
+// View EngineにEJSを指定。
+app.set('view engine', 'ejs');
+
+// "/"へのGETリクエストでindex.ejsを表示する。拡張子（.ejs）は省略されていることに注意。
+app.get("/", function(req, res, next){
+    res.render("index", {});
+});
+
+/* 2. listen()メソッドを実行して3000番ポートで待ち受け。*/
+var server = app.listen(3000, function(){
+    console.log("Node.js is listening to PORT:" + server.address().port);
+});
